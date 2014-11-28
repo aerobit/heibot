cccb89223a6f32291270f10b71bad41bc9ae5ef6
Michael Zhang <failed.down@gmail.com>
2014-11-28 16:13:37 -0600

    aaa

--- heibot.py
+++ heibot.py
@@ -2,6 +2,8 @@ import sys;
 import socket;
 import string;
 
+global heicounter;
+
 HOST = "irc.mibbit.net";
 PORT = 6667;
 NICK = "heibot";
@@ -11,6 +13,8 @@ s = socket.socket();
 s.connect((HOST, PORT));
 s.send("NICK "+NICK+"\n");
 s.send("USER "+NICK+" "+NICK+" "+NICK+" :"+NICK+"\n");
+
+heicounter = 0;
 
 connected = False;
 stop = False;
@@ -22,12 +26,17 @@ irc = {
 def parse(line):
     if line.find("heibot leave") != -1:
         s.send("QUIT\n");
-    elif line.find("hei") != -1:
+    else:
         if len(line.split(":")) == 3:
             username = line.split(":")[1].split("!")[0];
             message = line.split(":")[2];
             if message.strip() in ["hi", "hei", "hello"]:
                 s.send("PRIVMSG %s :hei, %s\n" % (irc['chan'], username));
+            if message.find("!") != -1:
+                actual = message[1:];
+                command = actual.split(" ")[0];
+                if command.find("count") != -1:
+                    print "nothing is done";
 
 while not stop:
     line = s.recv(1024);
