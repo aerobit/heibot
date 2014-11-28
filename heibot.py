c7626ef33378f57e0a054422bcffcbac0de9f1cc
Michael Zhang <failed.down@gmail.com>
2014-11-28 15:52:14 -0600

    now responds to greetings

--- heibot.py
+++ heibot.py
@@ -14,6 +14,7 @@ s.send("USER "+NICK+" "+NICK+" "+NICK+" :"+NICK+"\n");
 
 connected = False;
 stop = False;
+greet = False;
 irc = {
     "chan": "#easyctf",
 }
@@ -21,18 +22,25 @@ irc = {
 def parse(line):
     if line.find("heibot leave") != -1:
         s.send("QUIT\n");
-        stop = True;
+    elif line.find("hei") != -1:
+        if len(line.split(":")) == 3:
+            username = line.split(":")[1].split("!")[0];
+            message = line.split(":")[2];
+            if message.strip() in ["hi", "hei", "hello"]:
+                s.send("PRIVMSG %s :hei, %s\n" % (irc['chan'], username));
 
 while not stop:
     line = s.recv(1024);
     print line;
-    if line.find("PRIVMSG") != 1:
+    if line.find("PRIVMSG %s" % irc['chan']) != 1:
         parse(line);
     if line.find("PING") != -1:
         s.send("PONG :" + line.split(":")[1]);
         if not connected:
             s.send("JOIN %(chan)s\n" % irc);
-            s.send("PRIVMSG %(chan)s hei\n" % irc);
+            if not greet:
+                greet = True;
+                s.send("PRIVMSG %(chan)s :hei\n" % irc);
     if stop:
         break;
 
