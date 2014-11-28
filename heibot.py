d5f7d5196dcb8c1796c8a0716bdf499a7b745e53
Michael Zhang <failed.down@gmail.com>
2014-11-28 15:19:28 -0600

    you can tell it to leave now

--- heibot.py
+++ heibot.py
@@ -12,10 +12,27 @@ s.connect((HOST, PORT));
 s.send("NICK "+NICK+"\n");
 s.send("USER "+NICK+" "+NICK+" "+NICK+" :"+NICK+"\n");
 
-while 1:
+connected = False;
+stop = False;
+irc = {
+    "chan": "#easyctf",
+}
+
+def parse(line):
+    if line.find("heibot leave") != -1:
+        s.send("QUIT\n");
+        stop = True;
+
+while not stop:
     line = s.recv(1024);
     print line;
+    if line.find("PRIVMSG") != 1:
+        parse(line);
     if line.find("PING") != -1:
         s.send("PONG :" + line.split(":")[1]);
-        s.send("JOIN #easyctf\n");
+        if not connected:
+            s.send("JOIN %(chan)s\n" % irc);
+            s.send("PRIVMSG %(chan)s hei\n" % irc);
+    if stop:
+        break;
 
