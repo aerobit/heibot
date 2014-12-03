c5139ed687c67c502d51372d7e0456a61ea5fe93
Ari Lotter <arilotter@gmail.com>
2014-12-03 14:26:51 -0500

    lole oops

--- heibot.py
+++ heibot.py
@@ -5,8 +5,8 @@ import random
 
 HOST = "irc.mibbit.net"
 PORT = 6667
-NICK = "geibot"
-CHANNEL = "#fuckyou"
+NICK = "heibot"
+CHANNEL = "#easyctf"
 
 words = [i for i in open("wordlist.txt").read().split("\n") if len(i)>4]
 outcomes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again ", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "no.", "START", "A", "B", "UP", "DOWN", "LEFT", "RIGHT", "SELECT"]
